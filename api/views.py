24738247d8f472065792aadf5a1479b25d2958d1
Saurabh Kumar Giri <skgvns8789@gmail.com>
2022-03-16 08:15:51 +0530

    Added real routes data

--- api/views.py
+++ api/views.py
@@ -4,5 +4,40 @@ from django.http import JsonResponse
 # Create your views here.
 
 def getRoutes(request):
-    data = "My API"
-    return JsonResponse(data, safe=False)
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {
+                'body': ""
+            },
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {
+                'body': ""
+            },
+            'description': 'Updates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an existing note'
+        }
+    ]
+    return JsonResponse(routes, safe=False)
