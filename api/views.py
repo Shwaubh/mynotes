560a606cd6cd7dfc594c47b1e09edfb303bfe1b3
Saurabh Kumar Giri <skgvns8789@gmail.com>
2022-03-16 10:23:02 +0530

    add: Note Serializer | URLs path | Implementation

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Note
+from .serializers import NoteSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
@@ -42,3 +45,15 @@ def getRoutes(request):
         }
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    notes_serialized = NoteSerializer(notes, many=True)
+    return Response(notes_serialized.data)
+
+@api_view(['GET'])
+def getNote(request, id):
+    note = Note.objects.get(id=id)
+    note_serialized = NoteSerializer(note)
+    return Response(note_serialized.data)
