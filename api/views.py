7f6bb8ac8b2a2349f4b58751327d0a8e6cbee12b
Saurabh Kumar Giri <skgvns8789@gmail.com>
2022-03-16 07:48:17 +0530

    add : Dummy Path | Implementation | app api config

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def getRoutes(request):
+    data = "My API"
+    return JsonResponse(data, safe=False)
